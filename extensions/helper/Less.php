30e9a979f99896c5114691fc4a903333541a1582
Olivier Louvignes <olivier@mg-crea.com>
2011-10-13 16:26:53 +0200

    Clean.

--- extensions/helper/Less.php
+++ extensions/helper/Less.php
@@ -6,9 +6,9 @@
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
-namespace less\extensions\helper;
+namespace li3_less\extensions\helper;
 
-//require(__DIR__ . '/../../libraries/lessphp/lessc.inc.php');
+require(LITHIUM_APP_PATH . '/libraries/lessphp/lessc.inc.php');
 
 use lithium\core\Libraries;
 
@@ -26,28 +26,30 @@ class Less extends \lithium\template\Helper{
 
 	}
 
-	public function generate($sourceFiles, $options = array()) {
-
-		if(!is_array($sourceFiles)) $sourceFiles = array($sourceFiles);
+	public function generate($source, $options = array()) {
+		if(is_string($options)) $options = array('target' => $options);
+		if(!is_array($source)) $source = array($source);
 
 		$defaults = array(
-			'source' => APP_CSS_PATH . DS . 'source',
-			'dest' => APP_CSS_PATH,
-			'file' => "core",
+			'sourceFolder' => APP_CSS_PATH,
+			'targetFolder' => APP_CSS_PATH,
+			'target' => 'core',
 			'set' => array()
 		);
 		$options += $defaults;
 
-		if(!preg_match('/\\' . DS . '$/', $options['source'])) $options['source'] .= DS;
-		if(!preg_match('/\\' . DS . '$/', $options['dest'])) $options['dest'] .= DS;
+		// Append DS to folders
+		if(!preg_match('/\\' . DS . '$/', $options['sourceFolder'])) $options['sourceFolder'] .= DS;
+		if(!preg_match('/\\' . DS . '$/', $options['targetFolder'])) $options['targetFolder'] .= DS;
 
-		$cssDest = $options['dest'] . $options['file'] . '.css';
-		if(is_file($cssDest)) @unlink($cssDest);
+		$targetFile = $options['targetFolder'] . $options['target'] . '.css';
+		if(is_file($targetFile)) unlink($targetFile);
 
-		foreach($sourceFiles as $sourceFile) {
-			if(!preg_match('/(.css|.less)$/is', $sourceFile)) $sourceFile .= '.less';
-			$less = new \lessc($options['source'] . $sourceFile);
-			file_put_contents($cssDest, $less->parse(null, $options['set']), FILE_APPEND);
+		foreach($source as $file) {
+			if(!preg_match('/(.css|.less)$/is', $file)) $file .= '.less';
+			//if(!is_file($options['sourceFolder'] . $file)) throw new \BadMethodCallException(sprintf('File %s does not exists.', $options['sourceFolder'] . $file));
+			$less = new \lessc($options['sourceFolder'] . $file);
+			file_put_contents($targetFile, $less->parse(null, $options['set']), FILE_APPEND);
 		}
 
 	}
